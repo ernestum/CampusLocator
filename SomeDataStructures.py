711e7ce3dcbb1f41564f3826ea342d13cdfef41e
Maximilian <maximilian@ernestus.de>
2012-05-06 00:31:39 +0200

    Doku for arne

--- SomeDataStructures.py
+++ SomeDataStructures.py
@@ -4,9 +4,13 @@ Created on Apr 15, 2012
 @author: maximilian
 '''
 
+#
+# !!! Ignore the first two classes for now. We do not need them for functionallity
+#
 
 class CellManager(dict):
-    
+    '''This class might later be our database of all the cells that were ever scanned.
+    It is basically a dictionary that maps mac adresses to Cell objects'''    
     def addCell(self, cell):
         if(cell in self):
             return
@@ -16,7 +20,8 @@ class CellManager(dict):
         return self[mac]
 
 class Cell:
-    
+    '''This class just holds information about a wlan network. It does not store
+    the quality of the network because that changes everytime we move around.'''
     def __init__(self, mac, long = None, lat = None, essid = None):
         self.mac = mac
         self.long = long
@@ -24,16 +29,23 @@ class Cell:
         self.essid = essid
             
     def __eq__(self, other):
+        '''Implementation of equals operator '==' '''
         return self.mac == other.mac
         
         
     def __ne(self, other):
+        '''Implementation of the not equals operator '!=' '''
         return self.mac != other.mac
             
     
     def __repr__(self):
+        '''gives a string representation of the Cell'''
         return self.mac
     
+        
+#
+#    !!! Here the interesting classes start. Ignore the boring stuff above
+#
         
 class WlanScan(dict):
 
@@ -50,13 +62,18 @@ class WlanScan(dict):
             return 0
 
 class Room():
-    """This Class represents a room at our university"""  
+    '''This Class represents a room at our university.
+    It mainly holds a name, and a list with WlanScans, that are associated with
+    this room'''
+      
     def __init__(self, name, location=None):
+        '''location is optional'''
         self.name = name
         self.location = location
         self.wlanScans = []
         
     def addWlanScan(self, scan):
+        '''Adds another WlanScan to be associated with this room'''
         self.wlanScans.append(scan)
         
     def distanceTo(self, wlanScan):
--- wicd-connecting.py
+++ wicd-connecting.py
@@ -10,14 +10,19 @@ import sys
 from wicd import misc
 from SomeDataStructures import WlanScan
 
+#This part does some dbus related import stuff that we do not understand
 if getattr(dbus, 'version', (0, 0, 0)) < (0, 80, 0):
     import dbus.glib
 else:
     from dbus.mainloop.glib import DBusGMainLoop
     DBusGMainLoop(set_as_default=True)
-            
+
+
 class EasyWICDInterface():
+    '''Prototype class of a interface to the WICD Daemon via dbus'''
+    
     def __init__(self):
+        #mainly dbus initialisation that we do not understand
         bus = dbus.SystemBus()
         try:
             self.wireless = dbus.Interface(bus.get_object('org.wicd.daemon', '/org/wicd/daemon/wireless'),
@@ -28,16 +33,21 @@ class EasyWICDInterface():
             sys.exit(3)
             
     def scan(self):
+        '''Initiates a scan of the wlan networks in the area. 
+        This may take some seconds and some battery!'''
         self.wireless.Scan(True)
        
     def getWlanScan(self):
+        '''Returns a WlanScan object that is filled up with the data 
+        from the last scan. This does not initiate a wlan scan!'''
         scan = WlanScan()
         for network_id in range(0, self.wireless.GetNumberOfNetworks()):
             scan.update({str(self.wireless.GetWirelessProperty(network_id, 'bssid')) : int(self.wireless.GetWirelessProperty(network_id, 'quality'))})
         return scan
         
-        
     def printList(self):
+        '''Does some prettyprinting just for demo and debugging'''
+        
         print '#\tBSSID\t\t\tChannel\tQuality\tESSID'
         for network_id in range(0, self.wireless.GetNumberOfNetworks()):
             print '%s\t%s\t%s\t%s\t%s' % (network_id,
